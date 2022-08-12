7d0e10a511314f0ec01f57c2492222056547e58c
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-08-12 12:46:54 -0300

    feat: tag contem sódio para lanches percentuais entre 20 e 40. req 26 ok

---
+++ challenges/desafio26.js
@@ -0,0 +1,5 @@
+db.produtos.updateMany({
+  valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $lt: 40, $gt: 20 } } } },
+  { $push: { tags: { $each: ["contém sódio"] } } });
+
+db.produtos.find({}, { nome: true, tags: true, _id: false });
